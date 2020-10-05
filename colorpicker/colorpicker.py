afc4ec0c07af2221d4f98f3a8e073b9d1c8f4dab
avazevfx <avazevfx@gmail.com>
2020-10-05 13:01:41 +0200

    v1.0

--- colorpicker/colorpicker.py
+++ colorpicker/colorpicker.py
@@ -9,98 +9,67 @@
 import sys
 import colorsys
 
-from PyQt5.QtCore import (QPoint, Qt)
+from PyQt5.QtCore import (QPoint, Qt, pyqtSignal)
 from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import (QApplication, QDialog, QGraphicsDropShadowEffect)
+from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow)
 
 
-from .ui_dark import Ui_ColorPicker as Ui_Dark
-from .ui_light import Ui_ColorPicker as Ui_Light
+from .ui_main import Ui_ColorPicker as Ui_Main
 
 
-class ColorPicker(QDialog):
+class ColorPicker(QWidget):
 
-    def __init__(self, lightTheme=False):
-        super(ColorPicker, self).__init__()
+    colorChanged = pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        super(ColorPicker, self).__init__(*args, **kwargs)
 
         # Call UI Builder function
-        if lightTheme: self.ui = Ui_Light()
-        else: self.ui = Ui_Dark()
+        self.ui = Ui_Main()
         self.ui.setupUi(self)
 
-        # Make Frameless
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
-        self.setWindowTitle("Color Picker")
-
-        # Add DropShadow
-        self.shadow = QGraphicsDropShadowEffect(self)
-        self.shadow.setBlurRadius(17)
-        self.shadow.setXOffset(0)
-        self.shadow.setYOffset(0)
-        self.shadow.setColor(QColor(0, 0, 0, 150))
-        self.ui.drop_shadow_frame.setGraphicsEffect(self.shadow)
-
         # Connect update functions
-        self.ui.hue.sliderMoved.connect(self.hsvChanged)
+        self.ui.hue.mouseMoveEvent = self.moveHueSelector
+        self.ui.hue.mousePressEvent = self.moveHueSelector
         self.ui.red.textEdited.connect(self.rgbChanged)
         self.ui.green.textEdited.connect(self.rgbChanged)
         self.ui.blue.textEdited.connect(self.rgbChanged)
         self.ui.hex.textEdited.connect(self.hexChanged)
 
-        # Connect window dragging functions
-        self.ui.title_bar.mouseMoveEvent = self.moveWindow
-        self.ui.title_bar.mousePressEvent = self.setDragPos
-        self.ui.window_title.mouseMoveEvent = self.moveWindow
-        self.ui.window_title.mousePressEvent = self.setDragPos
-
         # Connect selector moving function
-        self.ui.black_overlay.mouseMoveEvent = self.moveSelector
-        self.ui.black_overlay.mousePressEvent = self.moveSelector
+        self.ui.black_overlay.mouseMoveEvent = self.moveSVSelector
+        self.ui.black_overlay.mousePressEvent = self.moveSVSelector
 
-        # Connect Ok|Cancel Button Box and X Button
-        self.ui.buttonBox.accepted.connect(self.accept)
-        self.ui.buttonBox.rejected.connect(self.reject)
-        self.ui.exit_btn.clicked.connect(self.reject)
-
-        self.lastcolor = (0,0,0)
         self.color = (0,0,0)
 
 
     ## Main Function ##
-    def getColor(self, lc=None):
-            if lc == None: lc = self.lastcolor
-            else: self.lastcolor = lc
-
-            self.setRGB(self.rgb2hsv(lc))
-            self.rgbChanged()
-            r,g,b = self.hsv2rgb(lc)
-            self.ui.lastcolor_vis.setStyleSheet(f"background-color: rgb({r},{g},{b})")
-
-            if self.exec_():
-                r, g, b = self.hsv2rgb(self.color)
-                return (r,g,b)
-
-            else:
-                return self.lastcolor
+    def getColor(self, mode="rgb"):
+        if mode == "rgb": color = self.hsv2rgb(self.color)
+        elif mode == "hsv": color = self.color
+        elif mode == "hex": color = self.hsv2hex(self.color)
+        else: raise ArgumentError ("Not a valid color mode. Modes are: 'rgb', 'hsv', 'hex'")
+        return color
 
 
     ## Update Functions ##
     def hsvChanged(self):
-        h,s,v = (self.ui.hue.value(), (self.ui.selector.x() + 6) / 2.0, (194 - self.ui.selector.y()) / 2.0)
+        h,s,v = (100 - self.ui.hue_selector.y() / 1.85, (self.ui.selector.x() + 6) / 2.0, (194 - self.ui.selector.y()) / 2.0)
         r,g,b = self.hsv2rgb(h,s,v)
         self.color = (h,s,v)
         self.setRGB(self.color)
         self.setHex(self.color)
         self.ui.color_vis.setStyleSheet(f"background-color: rgb({r},{g},{b})")
         self.ui.color_view.setStyleSheet(f"border-radius: 5px;background-color: qlineargradient(x1:1, x2:0, stop:0 hsl({h}%,100%,50%), stop:1 #fff);")
+        self.colorChanged.emit()
 
     def rgbChanged(self):
-        r,g,b = int(self.ui.red.text()), int(self.ui.green.text()), int(self.ui.blue.text())
+        r,g,b = self.i(self.ui.red.text()), self.i(self.ui.green.text()), self.i(self.ui.blue.text())
         self.color = self.rgb2hsv(r,g,b)
         self.setHSV(self.color)
         self.setHex(self.color)
         self.ui.color_vis.setStyleSheet(f"background-color: rgb({r},{g},{b})")
+        self.colorChanged.emit()
 
     def hexChanged(self):
         hex = self.ui.hex.text()
@@ -109,16 +78,17 @@ class ColorPicker(QDialog):
         self.setHSV(self.color)
         self.setRGB(self.color)
         self.ui.color_vis.setStyleSheet(f"background-color: rgb({r},{g},{b})")
+        self.colorChanged.emit()
 
 
     def setRGB(self, c):
         r,g,b = self.hsv2rgb(c)
-        self.ui.red.setText(str(int(r)))
-        self.ui.green.setText(str(int(g)))
-        self.ui.blue.setText(str(int(b)))
+        self.ui.red.setText(str(self.i(r)))
+        self.ui.green.setText(str(self.i(g)))
+        self.ui.blue.setText(str(self.i(b)))
 
     def setHSV(self, c):
-        self.ui.hue.setValue(c[0])
+        self.ui.hue_selector.move(7, (100 - c[0]) * 1.85)
         self.ui.color_view.setStyleSheet(f"border-radius: 5px;background-color: qlineargradient(x1:1, x2:0, stop:0 hsl({c[0]}%,100%,50%), stop:1 #fff);")
         self.ui.selector.move(c[1] * 2 - 6, (200 - c[2] * 2) - 6)
 
@@ -159,19 +129,8 @@ class ColorPicker(QDialog):
         else: h = h_or_color
         return self.rgb2hex(self.hsv2rgb(h,s,v))
 
-
-    ## Dragging Functions ##
-    def setDragPos(self, event):
-        self.dragPos = event.globalPos()
-
-    def moveWindow(self, event):
-        # MOVE WINDOW
-        if event.buttons() == Qt.LeftButton:
-            self.move(self.pos() + event.globalPos() - self.dragPos)
-            self.dragPos = event.globalPos()
-            event.accept()
-
-    def moveSelector(self, event):
+    # selector move function
+    def moveSVSelector(self, event):
         if event.buttons() == Qt.LeftButton:
             pos = event.pos()
             if pos.x() < 0: pos.setX(0)
@@ -181,8 +140,14 @@ class ColorPicker(QDialog):
             self.ui.selector.move(pos - QPoint(6,6))
             self.hsvChanged()
 
+    def moveHueSelector(self, event):
+        if event.buttons() == Qt.LeftButton:
+            pos = event.pos().y() - 7
+            if pos < 0: pos = 0
+            if pos > 185: pos = 185
+            self.ui.hue_selector.move(QPoint(7,pos))
+            self.hsvChanged()
 
-if __name__ == "__main__":
-    app = QApplication([])
-    cp = ColorPicker()
-    cp.getColor()
+    def i(self, text):
+        try: return int(text)
+        except: return 0
